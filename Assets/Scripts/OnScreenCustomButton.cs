c631297b8aa58d8a9f01dac524552374ab9d80a7
odaznara99 <jrodaz66@gmail.com>
2026-03-29 22:33:52 +0800

    added onscreen custom buttons

---
+++ Assets/Scripts/OnScreenCustomButton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class OnScreenCustomButton : MonoBehaviour
+{
+    private PlayerControllerVersion2 playerController;
+     void Awake()
+    {
+        playerController = FindAnyObjectByType<PlayerControllerVersion2>();
+        if (playerController == null)
+        {
+            Debug.LogError("PlayerControllerVersion2 not found in the scene.");
+        }
+    }
+
+    public void OnPlayerJump() { 
+        playerController.OnJump();
+    }
+    public void OnPlayerAttack() { 
+        playerController.OnHoldAttack();
+    }
+    public void OnPlayerShieldPressed() { 
+        playerController.OnHoldShield();
+    }
+
+    public void OnPlayerShieldRelease()
+    {
+        playerController.OnNeutral();
+    }
+    public void OnPlayerRoll() { 
+        playerController.OnRoll();
+    }
+    public void OnPlayerInteract() { 
+        //playerController.OnInteract();
+    }
+}
